93e1ba4e06d7bd97fc21677c2b7b08c760eec0bf
Jonas <jonas.e.andersson@gmail.com>
2025-12-11 15:32:56 +0100

    Add first agent functionality for service catalog

--- group1/agents/service_catalog/agent.py
+++ group1/agents/service_catalog/agent.py
@@ -1,1 +1,38 @@
-# This file will contain the implementation of the Service Catalog Agent.
+import os
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+client = MultiServerMCPClient({  # type: ignore
+    "service_catalog": {
+        "transport": "http",
+        "url": "http://0.0.0.0:8003/mcp"
+    }
+})
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    base_url=os.environ["OPENAI_BASE_URL"],
+    api_key=lambda: os.environ["OPENAI_API_KEY"],
+)
+
+
+async def get_agent():
+    # Create the agent
+
+    tools = await client.get_tools()
+    return create_agent(
+        model=llm,
+        tools=tools
+    )
+
+
+if __name__ == "__main__":
+    # Example of how to run the agent
+    agent = await get_agent()
+    response = agent.invoke({"input": "What services are available in the 'data' category?"})
+    print(response)
