0ddffc25f6798d5a543e8ceb0f73f7aac63356cb
Kerstin Johnsson <kerstin.johnsson@skane.se>
2025-12-11 16:20:02 +0100

    update employee_catalog agent

--- group1/agents/employee_catalog/agent.py
+++ group1/agents/employee_catalog/agent.py
@@ -1,11 +1,13 @@
+import asyncio
 import os
 
+import langchain.messages
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_openai import ChatOpenAI
 
-load_dotenv()
+load_dotenv(override=True)
 
 client = MultiServerMCPClient({  # type: ignore
     "employee_catalog": {
@@ -15,7 +17,8 @@ client = MultiServerMCPClient({  # type: ignore
 })
 
 llm = ChatOpenAI(
-    model="gpt-4o-mini",
+    model="gpt-5.1",
+    reasoning_effort='minimal',
     base_url=os.environ["OPENAI_BASE_URL"],
     api_key=lambda: os.environ["OPENAI_API_KEY"],
 )
@@ -31,8 +34,24 @@ async def get_agent():
     )
 
 
-if __name__ == "__main__":
+async def main():
     # Example of how to run the agent
     agent = await get_agent()
-    response = agent.invoke({"input": "What is the name of the employee with id 1?"})
-    print(response)
+    base_messages = await client.get_prompt("employee_catalog", "base_prompt")
+    messages = [
+        langchain.messages.SystemMessage("""\
+Du är en hjälpsam assistent som har som uppgift att hämta och spara uppgifter i
+Sjukhus ABCs personalkatalog. Ha en vänlig och hjälpsam ton och svara om möjligt på
+Skånska.
+"""),
+        * base_messages,
+        # {'role': 'user', 'content': "What is the name of the employee with id 1?"},
+        langchain.messages.HumanMessage("What is the name of the employee with id 1?")
+    ]
+
+    async for event in agent.astream_events({'messages': messages}):
+        print(event)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
